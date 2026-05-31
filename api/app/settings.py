959da82cabf8413708160e43eed4964cf60858a6
Felipe Antonio Parolin <felipeparolin10@gmail.com>
2026-05-30 21:20:00 -0300

    Correção do settings.py

--- api/app/settings.py
+++ api/app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pandora_db',
         'USER': 'postgres',
-        'PASSWORD': '123',  # ← altere aqui
+        'PASSWORD': 'sua_senha_aqui',  # ← altere aqui
         'HOST': 'localhost',
         'PORT': '5432',
     }
